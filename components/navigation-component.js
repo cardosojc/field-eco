61bf66d83ab6aa169506915d049013cd03868dbc
José Cardoso <jose.cardoso@digitalhub.man>
2020-06-30 20:05:57 +0100

    Rename components

---
+++ components/navigation-component.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { FieldDataScreen } from "./field-data-component";
+import { DetailsScreen } from "./details-component";
+
+const { Navigator, Screen } = createStackNavigator();
+
+const HomeNavigator = () => (
+  <Navigator headerMode="none">
+    <Screen name="FielData" component={FieldDataScreen} />
+    <Screen name="Details" component={DetailsScreen} />
+  </Navigator>
+);
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <HomeNavigator />
+  </NavigationContainer>
+);
